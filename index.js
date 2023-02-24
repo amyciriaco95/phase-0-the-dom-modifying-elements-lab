8b672498aca245dbd89bbb5b9917c89885a10206
amyciriaco95 <amyciriaco95@gmail.com>
2023-02-23 19:19:46 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+//1. remove <main> with id 'main':
+
+const removeMain = document.getElementById("main");
+removeMain.remove()
+
+//2. 'newHeader' variable that points to the <h1> node:
+const newHeader = document.createElement("h1");
+
+//3. 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.setAttribute("id", "victory");
+
+//4. 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it:
+newHeader.textContent = 'Amy is the champion';
